aa37673445a27f32ee3c3c15863e9217be6ad84c
Hidde Wieringa <hidde@hiddewieringa.nl>
2021-04-11 17:55:30 +0200

    Publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.32"
     id("fr.brouillard.oss.gradle.jgitver") version "0.9.1"
+    `maven-publish`
 }
 
 group = "nl.hiddewieringa"
@@ -25,5 +26,36 @@ tasks {
     }
     withType<Test> {
         useJUnitPlatform()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+                name.set("Money Kotlin")
+                description.set("Kotlin extensions for javax.money (Moneta) JSR 354")
+                url.set("https://github.com/hiddewie/money-kotlin")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hiddewie")
+                        name.set("Hidde Wieringa")
+                        email.set("hidde@hiddewieringa.nl")
+                    }
+                }
+                scm {
+                    developerConnection.set("scm:git:ssh://git@github.com:hiddewie/money-kotlin.git")
+                    url.set("https://github.com/hiddewie/money-kotlin.git")
+                }
+            }
+
+            from(components["java"])
+        }
     }
 }
